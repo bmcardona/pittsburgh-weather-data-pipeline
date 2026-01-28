625403711f2203dfb7d8c7c80206c24707a68a8b
Bradley <bradleymathematics@gmail.com>
2026-01-28 14:36:37 -0500

    create intermediate view to join current weather and locations

---
+++ dbt/models/intermediate/int_weather_joined_locations.sql
@@ -0,0 +1,34 @@
+with 
+
+current_weather as (
+
+    select * from {{ ref('stg_postgres__current_weather') }}
+    
+),
+
+locations as (
+
+    select * from {{ ref('stg_postgres__locations') }}
+
+),
+
+current_weather_and_locations as (
+    select
+        cw.*,
+        l.neighborhood_name,
+        l.community_board,
+        l.latitude,
+        l.longitude,
+        l.timezone,
+        l.created_at,
+        l.updated_at
+
+    from current_weather as cw
+
+    join locations as l
+    
+    on cw.location_id = l.location_id
+
+)
+
+select * from current_weather_and_locations
